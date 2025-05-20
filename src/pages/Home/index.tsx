cfdbec67b20e6ea9778fbc103a94a1ad0ad35f44
paulo-beltramin <dev.beltramin@gmail.com>
2025-05-20 19:46:41 -0300

    desenvolvido logica para criar links para as redes sociais, ao clicar abre em uma nova aba

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router'
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
 import { useEffect, useState } from 'react'
 import type { linkProps } from '../Admin'
-import { collection, getDocs, query } from 'firebase/firestore'
+import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore'
 import { db } from '../../Components/Services/db'
 
 type socialProps = {
@@ -17,30 +17,44 @@ export const Home = () => {
 
 
     useEffect(() => {
+        getLinks()
+        getSociais()
+    }, [])
 
-        const getLinks = () => {
-            const docRef = collection(db, "links")
 
-            const getLink = query(docRef)
-            const list = [] as linkProps[]
-            getDocs(getLink)
-                .then((item) => {
-                    item.forEach((doc) => {
-                        list.push({
-                            id: doc.id,
-                            nameLink: doc.data().nameLink,
-                            color: doc.data().color,
-                            background: doc.data().background,
-                            url: doc.data().url
-                        })
+    const getLinks = () => {
+        const docRef = collection(db, "links")
+
+        const getLink = query(docRef)
+        const list = [] as linkProps[]
+        getDocs(getLink)
+            .then((item) => {
+                item.forEach((doc) => {
+                    list.push({
+                        id: doc.id,
+                        nameLink: doc.data().nameLink,
+                        color: doc.data().color,
+                        background: doc.data().background,
+                        url: doc.data().url
                     })
                 })
+                setLinks(list)
+            })
+    }
 
-            setLinks(list)
-        }
 
-        getLinks()
-    }, [])
+    const getSociais = () => {
+        getDoc(doc(db, "social", "link"))
+            .then((item) => {
+                setSocialLinks({
+                    facebook: item.data()?.facebook,
+                    instagram: item.data()?.instagram,
+                    yotube: item.data()?.youtube
+                })
+            })
+
+    }
+
 
     return (
         <>
@@ -51,31 +65,35 @@ export const Home = () => {
                 {links && links.map((item) => (
 
 
-                    <>
-                        <Link to={`${item.url}`} className='sm:w-2xs md:w-2xl mx-auto my-0 p-2 rounded-md text-lg mb-5' target='_blank' style={{ background: item.background, color: item.color }}>
-                            <span>
+                    <div key={item.id} className='mb-4  w-2xl my-0 mx-auto ' >
+                        <Link to={`${item.url}`} target='_blank'>
+                            <span style={{ background: item.background, color: item.color }} className='py-1 px-4 rounded-lg block '>
                                 {item.nameLink}
                             </span>
                         </Link>
-                    </>
+                    </div>
                 )
 
                 )}
 
-                <section className='flex justify-center gap-4 text-white text-2xl'>
-                    <Link to={''} target='_blank'>
-                        <FaFacebook />
-                    </Link>
+                {socialLinks && Object.keys(socialLinks).length > 0 && (
+                    <>
+                        <footer className='flex justify-center gap-4 text-white text-2xl'>
+                            <Link to={socialLinks.facebook} target='_blank'>
+                                <FaFacebook />
+                            </Link>
 
-                    <Link to={''} target='_blank'>
-                        <FaInstagram />
-                    </Link>
+                            <Link to={socialLinks.instagram} target='_blank'>
+                                <FaInstagram />
+                            </Link>
 
-                    <Link to={''} target='_blank'>
-                        <FaYoutube />
-                    </Link>
-                </section>
+                            <Link to={socialLinks.yotube} target='_blank'>
+                                <FaYoutube />
+                            </Link>
+                        </footer>
 
+                    </>
+                )}
             </div>
         </>
     )
